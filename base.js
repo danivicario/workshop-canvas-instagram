9a6469929fccd82f56c7c66ae0d2eb34dc72d027
Dani Vicario <danivicario@dvicario.es>
2020-11-06 16:36:44 +0100

    added

---
+++ base.js
@@ -0,0 +1,66 @@
+// Dani Vicario - instagram experiment (canvas)- Thu 5 Nov 2020 00:22:08 CET
+
+// eslint-disable-next-line no-unused-vars
+const globalCompositeOperationModes = {
+  "normal": "source-over",
+  "source-in": "source-in",
+  "source-out": "source-out",
+  "source-atop": "source-atop",
+  "destination-over": "destination-over",
+  "destination-in": "destination-in",
+  "destination-out": "destination-out",
+  "destination-atop": "destination-atop",
+  "lighter": "lighter",
+  "copy": "copy",
+  "xor": "xor",
+  "multiply": "multiply",
+  "screen": "screen",
+  "overlay": "overlay",
+  "darken": "darken",
+  "lighten": "lighten",
+  "color-dodge": "color-dodge",
+  "color-burn": "color-burn",
+  "hard-light": "hard-light",
+  "soft-light": "soft-light",
+  "difference": "difference",
+  "exclusion": "exclusion",
+  "hue": "hue",
+  "saturation": "saturation",
+  "color": "color",
+  "luminosity": "luminosity"
+};
+
+/** @type HTMLCanvasElement */
+const canvasDOMEl = document.getElementById("canvas");
+
+/** @type CanvasRenderingContext2D */
+const ctx = canvasDOMEl.getContext("2d");
+
+function randomFloat(min, max) {
+  return Math.random() * (max - min) + min;
+}
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+let w, h, w2, h2;
+
+// eslint-disable-next-line no-unused-vars
+const { PI } = Math;
+// eslint-disable-next-line no-unused-vars
+const PI_DOUBLE = 2 * Math.PI;
+// eslint-disable-next-line no-unused-vars
+const PI_HALF = Math.PI / 2;
+
+canvasDOMEl.setAttribute("height", window.innerHeight);
+canvasDOMEl.setAttribute("width", window.innerWidth);
+
+w = window.innerWidth;
+h = window.innerHeight;
+w2 = w / 2;
+h2 = h / 2;
+
+ctx.save();
